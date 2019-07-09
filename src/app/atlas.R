61b61f60da33a11b292834775862ea50d497ea3c
Danielle Grogan <dgrogan@yukon.sr.unh.edu>
2019-07-09 13:57:09 -0400

    finished atlas figures

--- src/app/atlas.R
+++ src/app/atlas.R
@@ -12,7 +12,6 @@ library(rgdal)
 ### Maps made below: 
 # irrigated areas
 # rice paddies
-# soil properties 
 # rice paddy percolation rates
 
 ### shapefiles for plotting
@@ -32,6 +31,22 @@ plot.dir = "figures/atlas"
 
 # irrigated area
 irr.area = raster("/net/nfs/bog/raid/data/dgrogan/MIRCA_data/WBM_format/IrrAreaFraction_MIRCA2000_05mn.nc")
+irr.area[irr.area == 0] = NA
+irr.area = mask(irr.area, basin.shape)
+
+plot.nm = "Irrigated_area_CellFrac.png"
+png(file.path(plot.dir, plot.nm),
+    height=1200, width=1600, res=140)
+par(mar=c(3, 3.2,0,0))
+plot(coastline.shadow, xlim = xl, ylim = yl, border='grey90', lwd=4)
+plot(coastline,        xlim = xl, ylim = yl, border='grey70', col='white',  lwd=1, add=T)
+plot(irr.area,   add=T, legend=F)
+plot(basin.shape, add=T, lwd=0.8)
+plot(irr.area,   add=T, box=F, axes=T, las=1,
+     legend.only=T, legend.width=0.4, horizontal=T, smallplot=c(0.08, 0.28, 0.27, 0.29),
+     axis.args=list(cex.axis=1),
+     legend.args=list(text='Irrigated Area', side=3, font=1, line=0.1, cex=1))
+dev.off()
 
 # irrigated rice paddies
 rice.1 = raster("/net/nfs/bog/raid/data/dgrogan/MIRCA_data/WBM_format/IrrCrop_fraction/IrrCrop3_sub1_fraction.nc")
@@ -60,4 +75,25 @@ plot(rice.area,   add=T, box=F, axes=T, las=1,
      legend.only=T, legend.width=0.4, horizontal=T, smallplot=c(0.08, 0.28, 0.27, 0.29),
      axis.args=list(cex.axis=1),
      legend.args=list(text='Harvested Rice Area', side=3, font=1, line=0.1, cex=1))
+dev.off()
+
+
+# rice paddy percolation rates
+rice.perc = raster("/net/nfs/zero/home/WBM_TrANS/data/RicePercolationRate.nc")
+rice.perc[rice.perc == 0] = NA
+rice.perc = rice.perc * (rice.area > 0)
+rice.perc = mask(rice.perc, basin.shape)
+
+plot.nm = "Rice_paddy_percolation_rate_mmDay.png"
+png(file.path(plot.dir, plot.nm),
+    height=1200, width=1600, res=140)
+par(mar=c(3, 3.2,0,0))
+plot(coastline.shadow, xlim = xl, ylim = yl, border='grey90', lwd=4)
+plot(coastline,        xlim = xl, ylim = yl, border='grey70', col='white',  lwd=1, add=T)
+plot(rice.perc,   add=T, legend=F)
+plot(basin.shape, add=T, lwd=0.8)
+plot(rice.perc,   add=T, box=F, axes=T, las=1,
+     legend.only=T, legend.width=0.4, horizontal=T, smallplot=c(0.08, 0.28, 0.27, 0.29),
+     axis.args=list(cex.axis=1),
+     legend.args=list(text='Rice Paddy Percolation Rate (mm/day)', side=3, font=1, line=0.1, cex=1))
 dev.off()
